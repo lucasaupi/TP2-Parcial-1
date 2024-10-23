37d8d80d2953a4fb3b650c6ad9c709faa02ba2a6
Lucas <lucas-aupi@hotmail.com>
2024-10-22 21:15:32 -0300

    remove node modules

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ const PORT = 8080;
 app.use(express.json());
 app.use('/api', transactionRoutes);
 
-app.listen(PORT, () => console.log(`Server MVC running...`))
-app.on("Error", (error) => console.log("Error ", error))
+app.listen(PORT, () => console.log(`Server MVC running...`));
+app.on("Error", (error) => console.log("Error ", error));
